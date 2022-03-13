5f5ba14129fe254cf03dad37bf665435eaae8d34
MOHAMMED <momotaha039@gmail.com>
2022-03-13 22:07:26 +0200

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ import 'Cubit/CubiteStates.dart';
    BlocOverrides.runZoned(
          () {
            runApp( MyApp());
-           //wallahe walad
          },
      blocObserver: MyBlocObserver(),
    );
